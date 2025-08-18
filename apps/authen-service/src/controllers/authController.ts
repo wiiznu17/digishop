64241693969e48621d07336b6a21780d56dc4244
653040441-7 <jiraphat.si@kkumail.com>
2025-08-18 13:33:05 +0700

    det not use import

--- apps/authen-service/src/controllers/authController.ts
+++ apps/authen-service/src/controllers/authController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express"
 import { User } from "../../../../packages/db/src/models/User"
 import { signToken, verifyToken } from "../utils/jwt"
-import { Op } from "sequelize"
-import { UserRole } from "@digishop/db/src/types/enum"
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body
