563678cd27984b44b377e82077758c5ca91bfb36
potae <wissanuray@gmail.com>
2025-08-19 17:06:47 +0700

    done with merchant profile upload

--- apps/merchant-service/src/routes/userRouter.ts
+++ apps/merchant-service/src/routes/userRouter.ts
@@ -1,11 +1,31 @@
 import { Router } from 'express'
-import { createStoreForUser, deleteUser, getMerchantProfile } from '../controllers/userController'
+import { createStoreForUser, deleteUser, getMerchantProfile, updateMerchantProfile } from '../controllers/userController'
 import { authenticate } from '../middlewares/middleware'
+import { upload } from '../middlewares/upload'
 
 const router = Router()
 
-router.get('/profile', authenticate, getMerchantProfile)
-router.post('/register', createStoreForUser)
-router.delete('/:id', deleteUser)
+router.get(
+  '/profile',
+  authenticate,
+  getMerchantProfile
+)
+
+router.post(
+  '/register',
+  createStoreForUser
+)
+
+router.delete(
+  '/:id',
+  deleteUser
+)
+
+router.put(
+  "/profile",
+  authenticate,
+  upload.array("images", 1) as any,
+  updateMerchantProfile
+)
 
 export default router
