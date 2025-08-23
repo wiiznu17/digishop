f6574fef16d9c82d5b6f9bd50b03826950973d30
potae <wissanuray@gmail.com>
2025-08-23 22:39:14 +0700

    rename order status

--- packages/db/src/seeders/20250721010101-seed-user.ts
+++ packages/db/src/seeders/20250721010101-seed-user.ts
@@ -5,7 +5,7 @@ export default {
     await queryInterface.bulkInsert('USERS', [
       {
         email: 'customer1@example.com',
-        password: 'hashed_password_1',
+        password: '1234',
         first_name: 'Customer',
         last_name: 'One',
         middle_name: 'God',
@@ -16,7 +16,7 @@ export default {
       },
       {
         email: 'merchant1@example.com',
-        password: 'hashed_password_2',
+        password: '1234',
         first_name: 'Merchant',
         last_name: 'Two',
         middle_name: 'Seller',
@@ -27,7 +27,7 @@ export default {
       },
       {
         email: 'merchant2@example.com',
-        password: 'hashed_password_3',
+        password: '1234',
         first_name: 'Merchant',
         last_name: 'Three',
         middle_name: 'Seller',
