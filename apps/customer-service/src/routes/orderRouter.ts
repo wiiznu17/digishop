b831640cc70b80c519aa3c5473c844130ef1bea6
653040441-7 <jiraphat.si@kkumail.com>
2025-08-27 09:05:46 +0700

    can go to payment

--- apps/customer-service/src/routes/orderRouter.ts
+++ apps/customer-service/src/routes/orderRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express'
-import { createOrder, deleteOrder, findUserOrder , findOrder, getShipping } from '../controllers/orderControllers'
+import { createOrder, deleteOrder, findUserOrder , findOrder, findShipping } from '../controllers/orderControllers'
 const router = Router()
 
+router.get('/shiptype', findShipping)
 router.get('/:id',findOrder)
 router.get('/user/:id', findUserOrder)
 router.post('/create',createOrder)
 router.patch('/cancel/:id', deleteOrder)
-router.get('/shippingType', getShipping)
-
 export default router
