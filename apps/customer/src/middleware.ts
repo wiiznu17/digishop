92300171bcaefe17a680df7e071b72e3194f575e
653040441-7 <jiraphat.si@kkumail.com>
2025-09-03 09:56:53 +0700

    add middleware,order failed , order status Detail

---
+++ apps/customer/src/middleware.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server"
+import { jwtVerify } from "jose"
+
+const PUBLIC_PATHS = ["/register", "/"]
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+  const token = req.cookies.get("token")?.value
+  if (PUBLIC_PATHS.includes(pathname)) {
+    if (token) {
+      try {
+        console.log("Token:", token)
+        console.log("JWT_SECRET:", process.env.JWT_SECRET)
+        const payload = await jwtVerify(
+          token,
+          new TextEncoder().encode(process.env.JWT_SECRET!)
+        )
+        if (payload.payload.role === "CUSTOMER") {
+          return NextResponse.redirect(new URL("/digishop", req.url))
+        } else {
+          return NextResponse.redirect(new URL("/register"))
+        }
+      } catch (error) {
+        console.log('token',error)
+        return NextResponse.next()
+      }
+    } else {
+      if (pathname === "/register") {
+        return NextResponse.redirect(new URL("/", req.url))
+      } else {
+        return NextResponse.next()
+      }
+    }
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url))
+  }
+
+  try {
+    await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET!))
+    return NextResponse.next()
+  } catch (error){
+    console.log('!token',error)
+    return NextResponse.redirect(new URL("/", req.url))
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+}
