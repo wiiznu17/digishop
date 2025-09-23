f7ba44edaed7619c8426fdb69a7b4d7963e4e907
653040441-7 <jiraphat.si@kkumail.com>
2025-09-23 10:55:46 +0700

    edit select address

--- apps/customer/src/components/addressCard.tsx
+++ apps/customer/src/components/addressCard.tsx
@@ -9,10 +9,10 @@ const rubik = Rubik({
   subsets: ["latin"],
   weight: "300"
 })
-interface AddressCard {
+interface AddressCardForSetting {
   item: Address;
 }
-export default function AddressCard({ item }: AddressCard) {
+export function AddressCardForSetting({ item }: AddressCardForSetting) {
   const formatAddress = (items: Address): string => {
     return [
       items.address_number,
@@ -101,6 +101,84 @@ export default function AddressCard({ item }: AddressCard) {
       <EditAddress 
       item={item} setEditAddShow={setEditAddShow} editAddShow={editAddShow}      
       />
+    </div>
+  );
+}
+
+interface AddressCardForOrderProps {
+  item: Address
+  select: Address
+}
+
+export function AddressCardForOrder({ item , select }: AddressCardForOrderProps) {
+  const formatAddress = (items: Address): string => {
+    return [
+      items.address_number,
+      items.building,
+      items.street,
+      items.subStreet,
+      items.district,
+      items.subdistrict,
+      items.province,
+      items.postalCode,
+      items.country
+    ]
+      .filter(Boolean)
+      .join(' ')
+  }
+  const [editAddShow,setEditAddShow] = useState(false)
+  const handleDelete = async(id: number| undefined) => {
+    const delData = await deleteAddress(id)
+    if(delData.data){
+      window.location.reload()
+    }
+  }
+  console.log(item.id,select.id)
+  return (
+    <div className={`relative border  rounded-2xl mb-3 p-4 ${item.id !== select.id? ' border-gray-400':''}`}>
+      <div className={`${item.id !== select.id? 'text-gray-400':''}`}
+      > 
+        <div className="flex">
+          <h5 className="font-bold mb-6 ">{item.addressType}</h5>
+        </div>
+        <div className="space-y-6">
+          <div className="flex items-center space-x-4">
+            <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center">
+              <User size={18} className="" />
+            </div>
+            <div className="flex-1">
+              <label className="block text-sm font-medium  mb-1">
+                Recipient Name
+              </label>
+              <p className="text-lg">{item.recipientName}</p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center">
+              <House size={18} className="text-gray-60" />
+            </div>
+            <div className="flex-1">
+              <label className="block text-sm font-medium  mb-1">
+                Address
+              </label>
+              <p className=" text-lg">
+                {formatAddress(item)}
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center">
+              <Phone size={18} className="" />
+            </div>
+            <div className="flex-1">
+              <label className="block text-sm font-medium mb-1">
+                Phone Number
+              </label>
+              <p className="text-lg">{item.phone}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
