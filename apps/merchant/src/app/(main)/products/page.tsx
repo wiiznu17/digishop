d2bacba53f30bd530147d25763164279d61ae1c7
Wissanu Rayayoi <wissanuray@gmail.com>
2025-10-15 20:56:59 +0700

    fix build error

--- apps/merchant/src/app/(main)/products/page.tsx
+++ apps/merchant/src/app/(main)/products/page.tsx
@@ -1,5 +1,8 @@
-// apps/merchant/src/app/(main)/products/page.tsx
 "use client"
+
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+export const fetchCache = "force-no-store"
 
 import { useCallback, useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
