b9b1d5f9e39eff175b943548bc9b2a860dc485be
Wissanu Rayayoi <wissanuray@gmail.com>
2025-10-17 11:50:41 +0700

    fix auth req

--- apps/portal/src/utils/requesters/authRequester.ts
+++ apps/portal/src/utils/requesters/authRequester.ts
@@ -1,15 +1,31 @@
 import axios from "@/lib/axios"
 import { setAccessToken } from "@/lib/tokenStore"
 
-export async function login(
-  email: string,
-  password: string
-): Promise<{ accessToken: string }> {
+// export async function login(
+//   email: string,
+//   password: string
+// ): Promise<{ accessToken: string }> {
+//   const res = await axios.post("/api/auth/login", { email, password })
+//   const data = res.data as { accessToken: string }
+//   console.log("access tk: ", data.accessToken)
+//   if (data?.accessToken) setAccessToken(data.accessToken)
+//   return data
+// }
+export async function warmMe() {
+  try {
+    await axios.get("/api/auth/access")
+  } catch {}
+}
+
+export async function login(email: string, password: string) {
   const res = await axios.post("/api/auth/login", { email, password })
-  const data = res.data as { accessToken: string }
-  console.log("access tk: ", data.accessToken)
-  if (data?.accessToken) setAccessToken(data.accessToken)
-  return data
+  const { accessToken } = res.data as { accessToken: string }
+  console.log("access tk: ", accessToken)
+  if (accessToken) {
+    setAccessToken(accessToken)
+    await warmMe()
+  }
+  return { accessToken }
 }
 
 export async function logout(): Promise<{ ok: boolean }> {
